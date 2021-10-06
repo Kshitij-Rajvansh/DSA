dc331ea9a4346609813bc3fad3aba8a3afa06747
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2021-10-06 13:59:26 +0530

    books and other artifacts

---
+++ Algorithms/SearchAlgorithms/Search.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace Algorithms.SearchAlgorithms
+{
+    public static class Search
+    {
+        public static void BinarySearch ()
+        {
+            Console.WriteLine("Hi from binary search!!!");
+        }
+    }
+}
